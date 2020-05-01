9cf6f9eef3361d95de8dfe52690b69a2d2ab9a26
srishti2007 <64143148+srishti2007@users.noreply.github.com>
2020-05-01 18:03:25 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -1,61 +1,74 @@
 class Box {
-    constructor(x, y) {
-      var options = {
-          'restitution':0.8,
-          'friction':0.3,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, 50, 50, options);
-      this.body.x=x;
-      this.body.y=y;
-      World.add(world, this.body);
+  constructor(x, y) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
     }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill(255);
-      rect(pos.y, pos.x, 50, 50);
-      
+    this.body = Bodies.circle(x, y, 10 , options);
+    this.body.x=x;
+    this.body.y=y;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var ang =this.body.angle;
+    push ();
+    translate(pos.x, pos.y);
+    rotate(ang);
+    rectMode(CENTER);
+    fill(255);
+    ellipse(0, 0, 50, 50);
+    pop();
+  }
+};
+
+class Pillars {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
     }
-  };
-  
-  class Pillars {
-    constructor(x, y) {
-      var options = {
-          'restitution':0.8,
-          'friction':0.3,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, 100, 300, options);
-      this.body.x=x;
-      this.body.y=y;
-      World.add(world, this.body);
+    this.body = Bodies.rectangle(x, y, 100, 300, options);
+    this.body.x=x;
+    this.body.y=y;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var ang =this.body.angle;
+    push ();
+    translate(pos.x, pos.y);
+    rotate(ang);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, 100, 300);
+    pop();
+  }
+};
+class main {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
     }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill(255);
-      rect(pos.y ,pos.x, 100, 300);
-      
-    }
-  };
-  class main {
-    constructor(x, y) {
-      var options = {
-          'restitution':0.8,
-          'friction':0.3,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, 250, 250, options);
-      this.body.x=x;
-      this.body.y=y;
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill(255);
-      rect(pos.x, pos.y, 250, 250);
-     
-    }
-  };
+    this.body = Bodies.rectangle(x, y, 250, 250, options);
+    this.body.x=x;
+    this.body.y=y;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var ang =this.body.angle;
+    push ();
+    translate(pos.x, pos.y);
+    rotate(ang);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, 250, 250);
+    pop();
+  }
+};
+
--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, box2,box3, box4, box5;
+var box1, box2;
 var pillar1, pillar2;
 var block;
 var ground;
+
 
 function setup(){
     var canvas = createCanvas(800,600);
@@ -16,16 +17,16 @@ function setup(){
         isStatic:true
     }
      ground = Bodies.rectangle(200, 570, 1000, 20, groundOptions);
-     World.add(world, ground);
-    box1 = new Box(205, 150);                      
-    box2 = new Box (550, 150);
-    box3 = new Box (270, 150);
-    box4 = new Box (390,150);
-    box5 = new Box (450, 150);
-    pillar1 = new Pillars (250,300);
-    pillar2 = new Pillars (500, 300);
-    block = new main (350, 300);
+    World.add(world, ground);
+
+    box1 = new Box(205, 300);                      
+    box2 = new Box (550, 300);
+
+    pillar1 = new Pillars (250,450);
+    pillar2 = new Pillars (500, 450);
     
+    block = new main (400, 500);
+  
 }
 
 function draw(){
@@ -37,11 +38,7 @@ function draw(){
     rect(ground.position.x, ground.position.y, 1200, 20);
     box1.display();
     box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
     pillar1.display();
     pillar2.display();
     block.display();
-    
 }
